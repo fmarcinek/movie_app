07e2034acd5ba2de440d7d3475fe1506cdf1d9c3
Filip Marcinek <filipmar@interia.eu>
2020-09-09 22:44:22 +0200

    Add login requirement to search view

--- website/views.py
+++ website/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import SearchForm, MovieItem
 from .services import get_movie_items, get_certain_movie_info
 
 
-class MovieItemListView(TemplateView):
+class MovieItemListView(LoginRequiredMixin, TemplateView):
     template_name = 'website/movieitem_list.html'
 
     def get_context_data(self, **kwargs):
@@ -16,7 +16,6 @@ class MovieItemListView(TemplateView):
         return context
 
 
-# @login_required
 def home(request):
     form = SearchForm()
     return render(request, 'website/home.html', {'form': form, 'page': '1'})
