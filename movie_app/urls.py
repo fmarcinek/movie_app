13437acf9219c8b87c8ea97d3d155afe27757c95
Filip Marcinek <filipmar@interia.eu>
2020-09-09 14:20:53 +0200

    Add basic stylesheets

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website import views as website_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', website_views.home, name='website-home'),
 ]
